0e0b860698ac59c801773481da254817ac8efbdd
sojin-p <luv2hong@gmail.com>
2023-09-19 10:14:18 +0900

    Gitignore

--- Week10/ViewController.swift
+++ Week10/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func callRequest() {
+        
+        let url = URL(string: "https://api.unsplash.com/search/photos?query=sky&client_id=\(key)")
+        
+        AF.request(url, method: .get).validate()
+            .responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 
