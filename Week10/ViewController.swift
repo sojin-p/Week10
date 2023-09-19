16f9132718feac4e4dda94220c544c4124633c52
sojin-p <luv2hong@gmail.com>
2023-09-19 09:56:32 +0900

    Initial Commit

---
+++ Week10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week10
+//
+//  Created by 박소진 on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
