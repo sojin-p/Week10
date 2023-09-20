d32a70a5a10e6e3dd9bb358997cf4e9e455e7763
sojin-p <luv2hong@gmail.com>
2023-09-20 12:14:30 +0900

    230920 수업 내용 - 사진 확대 축소 더블탭

--- Week10/Network(Unsplash)/ViewController.swift
+++ Week10/Network(Unsplash)/ViewController.swift
@@ -6,50 +6,95 @@
 //
 
 import UIKit
+import Kingfisher
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var scrollView = {
+        let view = UIScrollView()
+        view.backgroundColor = .cyan
+        view.minimumZoomScale = 1
+        view.maximumZoomScale = 3 //보통 3~5
+        view.showsVerticalScrollIndicator = false
+        view.showsHorizontalScrollIndicator = false
+        view.delegate = self
+        return view
+    }()
+    
+    private let imageView = {
+        let view = UIImageView(frame: .zero)
+        view.backgroundColor = .clear
+        view.contentMode = .scaleAspectFit
+        view.isUserInteractionEnabled = true //제스처 받기
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
+        
+        configureHierarchy()
+        configureLayout()
+        configureGesture()
+        request()
+        
+    }
+    
+    func request() {
         Network.shared.requestConvertible(type: PhotoResult.self, api: .random) { response in
             switch response {
             case .success(let success):
                 dump(success)
+                
+                self.imageView.kf.setImage(with: URL(string: success.urls.thumb)!)
+                
             case .failure(let failure):
                 print(failure.errorDescription)
             }
         }
+    }
+    
+    private func configureGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapGesture))
+        tap.numberOfTapsRequired = 2 //몇번의 탭을 요구할건지
+        imageView.addGestureRecognizer(tap)
+    }
+    
+    @objc private func doubleTapGesture() {
+        if scrollView.zoomScale == 1 {
+            scrollView.setZoomScale(2, animated: true)
+        } else {
+            scrollView.setZoomScale(1, animated: true)
+        }
+    }
+    
+    private func configureLayout() {
         
-//        Network.shared.callRequest(type: PhotoResult.self, api: .detailPhoto(id: "4RJlMZl5KJ4")) { response in
-//            switch response {
-//            case .success(let success):
-//                dump(success)
-//            case .failure(let failure):
-//                print(failure.errorDescription)
-//            }
-//        }
+        scrollView.snp.makeConstraints { make in
+            make.center.equalTo(view)
+            make.size.equalTo(250)
+        }
         
-//        NetworkBasic.shared.randomRequest { photo, error in
-//            guard let photo = photo else { return }
-//            guard let error = error else { return }
-//        }
-        
-        
-//        NetworkBasic.shared.detailPhotoRequest(id: "QbF0TVjCqXs") { response in
-//            switch response {
-//            case .success(let success):
-//                print(success) //배열에 담고 테이블 뷰 갱신
-//            case .failure(let failure):
-//                print(failure) //다시 시도해주세요 얼럿, 토스트
-//            }
-//        }
-//
-//        NetworkBasic.shared.callRequest(query: "sky") { <#Result<Photo, Error>#> in
-//            <#code#>
-//        }
+        imageView.snp.makeConstraints { make in
+            make.size.equalTo(scrollView)
+        }
+    }
+    
+    private func configureHierarchy() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
     }
 
+}
+
+extension ViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? { //줌하면 어떻게 할까?
+        return imageView //이미지뷰 넣어주면 돼
+    }
+    
 }
 
 //Codable = 디코더블 인코더블 같이 갖고 있는 것 (인코더블은 우리가 만든 것을 외부로 보내는 거라 디코더블만 채택)
---
+++ Week10/Network(Unsplash)/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  Week10
+//
+//  Created by 박소진 on 2023/09/20.
+//
+
+import Foundation
+
+final class ViewModel {
+    
+}
